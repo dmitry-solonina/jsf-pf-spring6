c03c74f11255f2db184a1f8b0307f44e19759e06
dmitry.solonina <dmitry.solonina@b-next.com>
2023-04-01 13:52:44 +0200

    fix

--- redeploy.bat
+++ redeploy.bat
@@ -3,7 +3,7 @@ set PATH=%CATALINA_HOME%\bin;%PATH%
 
 del /Q /S C:\java\apache-tomcat\webapps\*
 rmdir /Q /S C:\java\apache-tomcat\webapps\demo
-copy demo\target\demo.war %CATALINA_HOME%\webapps\
+copy target\*.war %CATALINA_HOME%\webapps\
 
 java -version
 startup
